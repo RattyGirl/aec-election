2ebe829c5e6d638f1e3c40027cc0f1f77d9e696e
Chloe Holmes <39513781+RattyGirl@users.noreply.github.com>
2023-04-03 18:33:11 +1000

    getting preload

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::io::Read;
-use suppaftp::{FtpError, FtpStream};
+use suppaftp::{FtpStream};
 use std::str;
 use minidom::Element;
-use rusqlite::{Connection, Result};
+use rusqlite::{Connection};
 use zip::ZipArchive;
+
+
+const NAMESPACE: &str = "urn:oasis:names:tc:evs:schema:eml";
 
 fn main() {
     let mut ftp_stream: FtpStream = FtpStream::connect("mediafeedarchive.aec.gov.au:21").unwrap();
@@ -21,33 +24,45 @@ fn main() {
 }
 
 fn reset_db(conn: &Connection) {
-    conn.execute("DROP TABLE IF EXISTS contests", ()).unwrap();
-    conn.execute("DROP TABLE IF EXISTS elections", ()).unwrap();
-    conn.execute("DROP TABLE IF EXISTS events", ()).unwrap();
-    conn.execute("CREATE TABLE events (\
-        id integer primary key,\
-        name text\
-    )", ()).unwrap();
-    conn.execute("CREATE TABLE elections (\
-        event_id integer,\
-        id text,\
-        date text,\
-        name text,\
-        category text,\
-        FOREIGN KEY(event_id) REFERENCES events(id),\
-        PRIMARY KEY (event_id, id)\
-    )", ()).unwrap();
-    conn.execute("CREATE TABLE contests (\
+    conn.execute("DROP TABLE IF EXISTS candidates", ()).unwrap();
+    // conn.execute("DROP TABLE IF EXISTS contests", ()).unwrap();
+    // conn.execute("DROP TABLE IF EXISTS elections", ()).unwrap();
+    // conn.execute("DROP TABLE IF EXISTS events", ()).unwrap();
+    // conn.execute("CREATE TABLE events (\
+    //     id integer primary key,\
+    //     name text\
+    // )", ()).unwrap();
+    // conn.execute("CREATE TABLE elections (\
+    //     event_id integer,\
+    //     id text,\
+    //     date text,\
+    //     name text,\
+    //     category text,\
+    //     FOREIGN KEY(event_id) REFERENCES events(id),\
+    //     PRIMARY KEY (event_id, id)\
+    // )", ()).unwrap();
+    // conn.execute("CREATE TABLE contests (\
+    //     event_id integer,\
+    //     election_id integer,\
+    //     id text,\
+    //     short_code text,\
+    //     name text,\
+    //     position text,\
+    //     number integer,\
+    //     FOREIGN KEY(event_id) REFERENCES events(id),\
+    //     FOREIGN KEY(election_id) REFERENCES elections(id),\
+    //     PRIMARY KEY (event_id, election_id, id)\
+    // )", ()).unwrap();
+    conn.execute("CREATE TABLE candidates (\
         event_id integer,\
         election_id integer,\
-        id text,\
-        short_code text,\
+        contest_id text,\
+        id integer,\
         name text,\
-        position text,\
-        number integer,\
+        profession text,\
         FOREIGN KEY(event_id) REFERENCES events(id),\
-        FOREIGN KEY(election_id) REFERENCES elections(id),\
-        PRIMARY KEY (event_id, election_id, id)\
+        FOREIGN KEY(election_id) REFERENCES elections(id),
+        FOREIGN KEY(contest_id) REFERENCES contests(id)\
     )", ()).unwrap();
 }
 
@@ -60,17 +75,50 @@ fn preload_data(connection: &Connection, mut ftp_stream: &mut FtpStream, event_i
     let mut events_string: String = String::new();
     zipfile.by_name(format!("xml/eml-110-event-{}.xml", event_id).as_str()).unwrap().read_to_string(&mut events_string).unwrap();
 
-    const NAMESPACE: &str = "urn:oasis:names:tc:evs:schema:eml";
+    let mut candidates_string: String = String::new();
+    zipfile.by_name(format!("xml/eml-230-candidates-{}.xml", event_id).as_str()).unwrap().read_to_string(&mut candidates_string).unwrap();
+
+    // get_all_races(events_string, &connection, event_id);
+    get_all_candidates(candidates_string, &connection, event_id);
+
+    ftp_stream.cwd("/").unwrap();
+}
+
+fn get_all_candidates(candidates_string: String, connection: &Connection, event_id: &str) {
+    let mut candidates_string: Vec<&str> = candidates_string.split("\n").collect();
+    candidates_string.remove(0);
+    let candidates_string = candidates_string.join("\n");
+    let root: Element = candidates_string.parse().unwrap();
+    let candidate_list = root.get_child("CandidateList", NAMESPACE).unwrap();
+
+    let elections = candidate_list.children().filter(|f| f.name().eq("Election"));
+    elections.for_each(|election| {
+        let election_id = election.get_child("ElectionIdentifier", NAMESPACE).unwrap().attr("Id").unwrap();
+
+        election.children().filter(|f| f.name().eq("Contest")).for_each(|contest| {
+            let contest_id = contest.get_child("ContestIdentifier", NAMESPACE).unwrap().attr("Id").unwrap();
+
+            contest.children().filter(|f| f.name().eq("Candidate")).for_each(|candidate| {
+                let candidate_id = candidate.get_child("CandidateIdentifier", NAMESPACE).unwrap().attr("Id").unwrap();
+                let candidate_name = candidate.get_child("CandidateIdentifier", NAMESPACE).unwrap().get_child("CandidateName", NAMESPACE).unwrap().text();
+                let candidate_profession = candidate.get_child("Profession", NAMESPACE).unwrap().text();
+
+                connection.execute("INSERT INTO candidates VALUES (?1, ?2, ?3, ?4, ?5, ?6)", (event_id, election_id, contest_id, candidate_id, candidate_name, candidate_profession)).unwrap();
+            });
+        });
+    })
+}
+
+fn get_all_races(events_string: String, connection: &Connection, event_id: &str) {
     let mut events_string: Vec<&str> = events_string.split("\n").collect();
     events_string.remove(0);
-    let candidates_string = events_string.join("\n");
-    let root: Element = candidates_string.parse().unwrap();
+    let events_string = events_string.join("\n");
+    let root: Element = events_string.parse().unwrap();
     let mut iter = root.children();
     let election_event = root.get_child("ElectionEvent", NAMESPACE).unwrap();
     let event_identifier = election_event.get_child("EventIdentifier", NAMESPACE).unwrap();
 
     let name = event_identifier.get_child("EventName", NAMESPACE).unwrap().text();
-
     connection.execute("INSERT into events VALUES (?1, ?2)", (event_id, event_identifier.get_child("EventName", NAMESPACE).unwrap().text())).unwrap();
 
     let elections = election_event.children().filter(|f| f.name().eq("Election"));
@@ -92,8 +140,6 @@ fn preload_data(connection: &Connection, mut ftp_stream: &mut FtpStream, event_i
         });
 
     });
-
-    ftp_stream.cwd("/").unwrap();
 }
 
 fn change_dir(mut stream: &mut FtpStream, x: &str) {
